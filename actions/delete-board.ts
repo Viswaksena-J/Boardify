a8801d810d5c3db1daaed355ef8f07fe149c1e75
Viswaksena Jayam <viswaksene111@gmail.com>
2024-04-08 00:44:16 +0530

    Server actions

---
+++ actions/delete-board.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export async function deleteBoard(id: string){
+    await db.board.delete({
+        where:{
+            id
+        }
+    });
+
+    
+    revalidatePath("/organization/org_2edCRQtHNey7122AIAFbj03QMJC")
+}
